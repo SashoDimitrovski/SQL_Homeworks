a3809ebd183f11b4152fb4218d4d3f97a5fd6433
Saso Dimitrovski <sasodimitrovski1982@yahoo.com>
2020-08-20 15:33:26 +0200

    fourth homework

---
+++ Session4/SQLQuery1_2.sql
@@ -0,0 +1,54 @@
+﻿use SEDCHome_2020_G7
+--Homework requirement 1/2
+--Declare scalar variable for storing FirstName values​
+--Assign value ‘Antonio’ to the FirstName variable​
+--Find all Students having FirstName same as the variable​
+
+declare @FirstName as nvarchar (30)
+set @FirstName = 'Antonio'
+select * from [dbo].[Student]
+where [FirstName] = @FirstName
+
+
+--Declare table variable that will contain StudentId, StudentName and DateOfBirth
+--Fill the table variable with all Female students
+declare @StudentInfo table 
+(StudentId int, StudentName nvarchar (30), DateOfBirth date)
+insert into @StudentInfo
+select [ID], [FirstName], [DateOfBirth]
+from [dbo].[Student]
+where Gender = 'F'
+
+select * from @StudentInfo
+
+
+--Declare temp table that will contain LastName and EnrolledDate columns​.
+--Fill the temp table with all Male students having First Name starting with ‘A’​
+--Retrieve the students from the table which last name is with 7 characters​
+
+create table #StudentInfo2
+(LastName nvarchar (30), EnrolledDate date)
+insert into #StudentInfo2
+select [FirstName], [DateOfBirth]
+from [dbo].[Student]
+
+select * from #StudentInfo2
+
+
+create table #StudentInfo3
+(LastName nvarchar (30), EnrolledDate date)
+insert into #StudentInfo3
+select [LastName], [DateOfBirth]
+from [dbo].[Student]
+where Gender = 'M'
+and FirstName like 'A%'
+
+select * from #StudentInfo3
+where Len([LastName]) = 7    -- tuka pak ne dava rezultati. Vidi kaj tebe dali ke dade
+
+--Find all teachers whose FirstName length is less than 5 and​
+--the first 3 characters of their FirstName and LastName are the same
+
+select * from [dbo].[Teacher]
+where  Len([FirstName]) < 5
+and Substring([FirstName],1,3) = Substring ([LastName], 1,3)
---
+++ Session4/SQLQuery2_2.sql
@@ -0,0 +1,21 @@
+﻿--Homework requirement 2/2​
+--Declare scalar function (fn_FormatStudentName) for retrieving the Student description for specific StudentId in the following format:​
+--StudentCardNumber without “sc-”​
+--“ – “​
+--First character of student FirstName​
+--“.”​
+--Student LastName​
+
+create or alter function fn_FormatStudentName (@StudentId int)
+returns nvarchar (50)
+as
+begin
+declare @Result nvarchar (50)
+select @Result = Right(StudentCardNumber, Len(StudentCardNumber-3) + '-' + SUBSTRING(FirstName,1,1) + '.' + LastName)
+from [dbo].[Student]
+where [ID] = @StudentId
+return @Result
+end
+
+select [dbo].[fn_FormatStudentName] (4) 
+-- Mi dava Conversion failed when converting the nvarchar value 'sc-82787' to data type int. 
